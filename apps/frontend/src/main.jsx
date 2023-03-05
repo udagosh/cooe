a83ca7461d5110bfed74e9b33b87557bf9eff1f7
Rahul Sharma <rahul32412@gmail.com>
2023-03-05 14:46:42 +0530

    Adding environmental variable

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain="dev-ewmrc8c2y6cijr6g.us.auth0.com"
-    clientId="RmN9TZCZ9kdL12u6EFkUh40tVaXmpqx6"
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
